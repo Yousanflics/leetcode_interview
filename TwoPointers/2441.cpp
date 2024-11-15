603feef37720549d7d8eadd3687a1ea58540d581
Zixi Yang <zyang792@wisc.edu>
2024-11-15 00:48:16 -0600

    update 2441 findMaxK

--- TwoPointers/2441.cpp
+++ TwoPointers/2441.cpp
@@ -4,7 +4,7 @@ public:
         sort(begin(nums), end(nums), [](int a, int b) { 
             return abs(a) == abs(b) ? a < b : abs(a) > abs(b);
             });
-        for(int i = 1; i < nums.size() - 1; i++) {
+        for(int i = 1; i < nums.size(); i++) {
             if(nums[i] == - nums[i - 1]) return nums[i];
         }
         return -1;
