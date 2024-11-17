7657015e8f19eee8948b68de55f7fe6a2a38b7c0
Zixi Yang <zyang792@wisc.edu>
2024-11-16 18:12:30 -0600

    add 76 code comments

--- 76.cpp
+++ 76.cpp
@@ -7,20 +7,24 @@ string 是 ascii 编码一共就 128 中，所以可以声明一个 size 是 128
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int> letterCnt(128) =  {0};
-        int left = 0, cnt = 0; minLeft = -1; minLen = INT_MAX;
-        for(char c : t) ++letterCnt[c];
-        for(int i = 0; i < s.size(), ++i) {
+        vector<int> letterCnt(128) = {0};
+        int left = 0, cnt = 0, minLeft = -1, minLen = INT_MAX;
+        for(const char & c : t) ++letterCnt[c];
+        for(int i = 0; i < s.size(); ++i) {
             if(--letterCnt[s[i]] >= 0) ++cnt;
-            while(cnt == t.size) {
+            // 缩小边界
+            while(cnt == t.size()) {
+                // 更新左边界&length
                 if(minLen > i - left + 1) {
                     minLen = i - left + 1;
+                    //left 起始为 0
                     minLeft = left;
                 }
+                //自增之后发现 > 0 说明回复了之后去除掉了本来有的元素，left可以++但是不能赋值给 minLeft 也正是因为 cnt != t.size() 了所以没法赋值
                 if(++letterCnt[s[left]] > 0) --cnt;
-                ++left;
+                left++;
             }
         }
-        return minLeft == -1 ? "" : s.substr(minLeft, minLen);
+        return minLeft == -1 ? "":s.substr(minLeft, minLen);
     }
 };
