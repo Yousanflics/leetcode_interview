f4d3a16fde33fb6c6767f6ea2dfbcbb8c2d89a33
Zixi Yang <zyang792@wisc.edu>
2024-11-17 22:05:35 -0600

    finish 637

---
+++ 637.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> res
+        queue<ListNode*> q = {{root}};
+        while(!q.empty()) {
+            int n = q.size();
+            for(int i = n; i > 0; --i){
+                int sum;
+                ListNode* t = q.front(); q.pop();
+                sum += t->val;
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+            }
+            res.push_back(sum/n);
+        }
+        return res;
+    }
+};
