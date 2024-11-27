44d14cce6aaefb45ea61fd3421f9088511d9b2f9
Zixi Yang <zyang792@wisc.edu>
2024-11-26 21:29:16 -0600

    update 105

--- BInaryTree/105.cpp
+++ BInaryTree/105.cpp
@@ -18,19 +18,18 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        return buildTree(preorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
-    TreeNode* buildTree(vector<int> &preoder, int pLeft, int pRight, vector<int> &inorder, int iLeft, int iRight){
-        // 传进来的直接就是 size() - 1 的故可以放心使用 <= 确定边界
-        if(pLeft > pRight || iLeft > iRight) return NULL;
+    TreeNode *buildTree(vector<int> &preorder, int pLeft, int pRight, vector<int> &inorder, int iLeft, int iRight) {
+        if (pLeft > pRight || iLeft > iRight) return NULL;
         int i = 0;
-        for(int i = iLeft; i <= iRight; ++i) {
-            if(preoder[pLeft] == inorder[i]) break;
+        for (i = iLeft; i <= iRight; ++i) {
+            if (preorder[pLeft] == inorder[i]) break;
         }
-        TreeNode *cur = new TreeNode(preoder[pLeft]);
-        cur->left = buildTree(preoder, pLeft + 1, pLeft + i - iLeft, inorder, iLeft, i - 1);
-        cur->right = buildTree(preoder, pLeft + i - iLeft + 1, pRight, inorder, i + 1, iRight);
+        TreeNode *cur = new TreeNode(preorder[pLeft]);
+        cur->left = buildTree(preorder, pLeft + 1, pLeft + i - iLeft, inorder, iLeft, i - 1);
+        cur->right = buildTree(preorder, pLeft + i - iLeft + 1, pRight, inorder, i + 1, iRight);
         return cur;
     }
 };
