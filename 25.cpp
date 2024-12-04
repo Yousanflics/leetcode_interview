2d640c29ee3d050a2a7702dce5fc585df27195e4
Zixi Yang <zyang792@wisc.edu>
2024-12-04 15:04:45 -0600

    finish 25

---
+++ 25.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *cur = head;
+        for (int i = 0; i < k; ++i) {
+            if (!cur) return head;
+            cur = cur->next;
+        }
+        ListNode *new_head = reverse(head, cur);
+        head->next = reverseKGroup(cur, k);
+        return new_head;
+    }
+    ListNode* reverse(ListNode* head, ListNode* tail) {
+        ListNode *pre = tail;
+        while (head != tail) {
+            ListNode *t = head->next;
+            head->next = pre;
+            pre = head;
+            head = t;
+        }
+        return pre;
+    }
+};
