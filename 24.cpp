1307b30c9dfc7c89144f2b62b412b990a72ef6cf
Zixi Yang <zyang792@wisc.edu>
2024-12-04 14:58:01 -0600

    finish 24

---
+++ 24.cpp
@@ -0,0 +1,29 @@
+/*
+本题是简单的对链表的操作题目可以考虑有递归和迭代，迭代需要一个 dummy head 最后返回这个就行
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *dummy = new ListNode(-1), *pre = dummy;
+        dummy->next = head;
+        while (pre->next && pre->next->next) {
+            ListNode *t = pre->next->next;
+            pre->next->next = t->next;
+            t->next = pre->next;
+            pre->next = t;
+            pre = t->next;
+        }
+        return dummy->next;
+    }
+};
