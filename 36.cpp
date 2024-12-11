2d1a97540d48f7adee5bc5b990680628a65b54cd
Zixi Yang <zyang792@wisc.edu>
2024-12-11 03:21:43 -0600

    finish 36

---
+++ 36.cpp
@@ -0,0 +1,21 @@
+/*
+最直接的暴力遍历 On^2 是时间复杂度最低的
+*/
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> st;
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] == '.') continue;
+                string t = "(" + to_string(board[i][j] - '0') + ")";
+                string row = to_string(i) + t, col = t + to_string(j), cell = to_string(i / 3) + t + to_string(j / 3);
+                if (st.count(row) || st.count(col) || st.count(cell)) return false;
+                st.insert(row);
+                st.insert(col);
+                st.insert(cell);
+            }
+        }
+        return true;
+    }
+};
