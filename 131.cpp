c5c6ca67b062711ed8899e7897fc6f17a4ac60de
Zixi Yang <zyang792@wisc.edu>
2025-02-26 20:29:14 -0600

    update comments

--- 131.cpp
+++ 131.cpp
@@ -10,6 +10,7 @@ public:
     void helper(string s, int start, vector<string>& out, vector<vector<string>>& res) {
         if(start == s.size()) { res.push_back(out); return; }
         for(int i = start; i < s.size(); ++i) {
+            // 这里的 continue 是跳过上面的 for 循环，如果不是回文就不递归了
             if(!isPalindrome(s, start, i)) continue;
             out.push_back(s.substr(start, i - start + 1));
             helper(s, i + 1, out, res);
