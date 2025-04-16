57681034d4019775f84a5b38f6768a18feff2c40
Zixi Yang <zyang792@wisc.edu>
2025-04-16 17:33:50 -0500

    add 695 max area of island

---
+++ Medium/Graph/695.py
@@ -0,0 +1,29 @@
+# 695 max area of island
+from typing import List
+"""
+这个跟之前的课程安排不太一样，课程安排是需要需要换，属于是定性，课程安排2是需要整个拓扑排序的完整顺序
+这个跟前面的两个又不一样，这是需要有一个 max_area 记录每次每个岛最大，当前的和之前的max的
+
+时间复杂度：O(m*n)，其中 m 和 n 分别是网格的行数和列数。最坏情况下，我们需要访问网格中的每个单元格一次。
+空间复杂度：O(mn)，最坏情况下，整个网格都是陆地，递归调用栈的深度将达到 mn。
+"""
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        max_area = 0
+        def dfs(i, j):
+            if not (0 <= i < m and 0 <= j < n) or grid != 1:
+                return 0
+            # 标记为已访问, 翻转一下
+            grid[i][j] = 0
+
+            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j-1) + dfs(i, j+1)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+        return max_area
