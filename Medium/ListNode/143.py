d73c5c53135248f41d02e744795fca0de25bf984
Zixi Yang <zyang792@wisc.edu>
2025-04-21 17:10:57 -0500

    add comments to 143

--- Medium/ListNode/143.py
+++ Medium/ListNode/143.py
@@ -3,11 +3,15 @@ class ListNode:
         self.val = val
         self.next = None
 
+# 时间复杂度为 O(n)
+# 空间复杂度为 O(1)
 class Solution:
     def reorderList(self, head: ListNode) -> None:
+        # 整个过程可以分为 3 步走
         if not head or not head.next or not head.next.next:
             return
         
+        # 第一步：找到链表重点
         slow, fast = head, head
         while fast.next and fast.next.next:
             slow = slow.next
@@ -16,7 +20,7 @@ class Solution:
         second_half = slow.next
         # breakup listnode
         slow.next = None
-
+        # 第二步：翻转第二部分链表
         prev = None
         current = second_half
         while current:
@@ -25,7 +29,7 @@ class Solution:
             prev = current
             current = next_temp
         reversed_second_half = prev
-
+        # 第三步：交叉合并两部分链表
         first = head
         second = reversed_second_half
         while second:
