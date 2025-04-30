996b499ce1f1be16bd7cdc79705b6e930bb7c25a
Zixi Yang <zyang792@wisc.edu>
2025-04-30 15:30:28 -0500

    add TreeNode for 235 adn update 300

--- Medium/DP/300.py
+++ Medium/DP/300.py
@@ -18,3 +18,31 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
         # dp[i] 对应的是 i 结尾的元素的最大的长度，所以找 dp 中的最大的那个数对应的就是最大长度
         return max(dp)
+    
+
+    # 新 solution dp+二分查找(NlogN)
+    def lengthOfLISBetter(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        # tails[i]表示长度为i+1的递增子序列的结尾元素的最小可能值
+        tails = []
+        # 二分的目的：
+        # 为当前处理的元素num找到 tails 数组中第一个大于等于它的位置，记为left
+        for num in nums:
+            # 二分查找确定插入的位置
+            left, right = 0, len(tails)
+            while left < right:
+                # 地板除向下取整
+                mid = (left+right) // 2
+                if tails[mid] < num:
+                    left = mid+1
+                else:
+                    right = mid
+            # 如果 num 大于所有 tails 中的值，则添加它
+            if left == len(tails):
+                tails.append(num)
+            else:
+                # 否则替换替换第一个大于等于 num 的值
+                tails[left] = num
+        return len(tails)
+
--- Medium/Tree/235.py
+++ Medium/Tree/235.py
@@ -1,3 +1,9 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if not root or not p or not q:
@@ -6,5 +12,16 @@ class Solution:
             return self.lowestCommonAncestor(root.left, p, q)
         elif(min(p.val, q.val) > root.val):
             return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+        
+    def lowestCommonAncestor1(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or not p or not q:
+            return None
+        if(max(p.val, q.val) < root.val):
+            return self.lowestCommonAncestor1(root.left, p, q)
+        elif(min(p.val, q.val) > root.val):
+            return self.lowestCommonAncestor1(root.right, p, q)
+        # p,q 在 root 两侧
         else:
             return root
