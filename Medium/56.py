69157f7d2bdb373302763844a8324f33a15af5f2
Zixi Yang <zyang792@wisc.edu>
2025-04-30 02:35:58 -0500

    update 56 comments

--- Medium/56.py
+++ Medium/56.py
@@ -5,6 +5,7 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
+        # 一定要首先基于第一个下标进行排序，否则不能使用后面的算法
         intervals.sort(key=lambda x: x[0])
         # 这里记得写中括号，不然不好对比
         merged = [intervals[0]]
