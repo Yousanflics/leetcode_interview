da6f705ca699b0fb7566a0230f1db7e64385d7a8
Zixi Yang <zyang792@wisc.edu>
2025-05-08 01:36:20 -0500

    update 670

--- Medium/670.py
+++ Medium/670.py
@@ -8,8 +8,9 @@ class Solution:
             last[digits[i]] = i
         for i in range(n):
             for d in range(9, digits[i], -1):
-                if d in last and digits[d] > i:
-                    j = digits[d]
+                #当前找到的最大的在 last 中且 last 位置在 i 的后面 last[d] > i，把 i 和 当前位置换了
+                if d in last and last[d] > i:
+                    j = last[d]
                     digits[i], digits[j] = digits[j], digits[i]
                     return int(''.join(map(str, digits)))
         return num
